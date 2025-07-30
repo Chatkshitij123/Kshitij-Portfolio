e13bd2c7ff0f8fe8023e334d8babeab963b591e1
Chatkshitij123 <kshitijchaturvedi265@gmail.com>
2025-07-30 10:55:52 +0530

    new app added

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,22 +1,16 @@
 import Work1 from "../../assets/loginpage.png";
-// import Work2 from "../../assets/work2.jpg";
+import Work2 from "../../assets/StreamifySignInPage.png";
 // import Work3 from "../../assets/work3.jpg";
 // import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/hullu-ott-app.png";
 export const projectsData = [
     {
         id: 1,
-        image: Work1,
-        title: 'Chat-App',
-        category: 'web',
-        link: 'https://chat-app1-6y2q.onrender.com/'
+        image: Work2,
+        title: 'Streamify',
+        category: 'Web',
+        link: 'https://streamify-jq33.onrender.com/login'
     },
-    // {
-    //     id: 2,
-    //     image: Work2,
-    //     title: 'App movil',
-    //     category: 'app',
-    // },
     // {
     //     id: 3,
     //     image: Work3,
@@ -35,6 +29,13 @@ export const projectsData = [
         title: 'Hullu-Ott-App',
         category: 'Web',
         link: 'https://hullu-app.netlify.app/'
+    },
+    {
+        id: 3,
+        image: Work1,
+        title: 'Chat-App',
+        category: 'web',
+        link: 'https://chat-app1-6y2q.onrender.com/'
     },
 ];
 export const projectsNav = [
